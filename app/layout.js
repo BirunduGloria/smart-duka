e9aef43b72ce973b310720301b34e51547c51d77
Ashington Munene <munenefrank11@gmail.com>
2025-07-24 11:18:44 +0300

    Remove unnecessary <html> tag from RootLayout component

--- app/layout.js
+++ app/layout.js
@@ -11,12 +11,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ContextProvider>
           {children}
         </ContextProvider>
       </body>
-    </html>
   );
 }

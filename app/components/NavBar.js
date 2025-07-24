43b0d382da1f656a09c7fe6603822f0cdf233210
peter munyambu <petermunyambu1000@gmail.com>
2025-07-24 16:10:43 +0300

    Fix navigation link: Correct casing of 'Products' in NavBar component

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -9,7 +9,7 @@ export default function NavBar() {
       <div className="nav-logo">SmartPOS</div>
       <ul className="nav-links">
         <li><Link href="/">Home</Link></li>
-        <li><Link href="/products">Products</Link></li>
+        <li><Link href="/Products">products</Link></li>
         <li><Link href="/cart">Cart</Link></li>
         <li><Link href="/inventory">Inventory</Link></li>
         <li><Link href="/login">Login</Link></li>

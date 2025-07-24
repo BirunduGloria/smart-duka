20e81fa1570ce714098f3d0888f50860fcb2bb08
peter munyambu <petermunyambu1000@gmail.com>
2025-07-24 16:10:25 +0300

    Fix navigation link: Update ProductCard link to point to Products page

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function NavBar() {
       <ul className="nav-links">
         <li><Link href="/">Home</Link></li>
         <li><Link href="/cart">Cart</Link></li>
-        <li><Link href="/productcard">ProductCard</Link></li>
+        <li><Link href="/products">Products</Link></li>
         <li><Link href="/login">Login</Link></li>
         <li><Link href="/inventory">Inventory</Link></li>
       </ul>

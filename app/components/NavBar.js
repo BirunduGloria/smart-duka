8c10abd149379a8770ea064b89ac904fbc541cd8
Ashington Munene <munenefrank11@gmail.com>
2025-07-24 08:40:52 +0300

    Refactor NavBar to conditionally render Inventory link for admin users

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,9 +1,13 @@
-// app/components/NavBar.js
+'use client';
 
+import { useContext } from 'react';
+import { UserContext } from '../../context/UserContext';
 import Link from "next/link";
-import "../globals.css"; // You can move styles to a separate CSS file if needed
+import "../globals.css";
 
 export default function NavBar() {
+  const { user } = useContext(UserContext); {/* imported user info*/}
+
   return (
     <nav className="navbar">
       <div className="nav-logo">SmartPOS</div>
@@ -11,7 +15,9 @@ export default function NavBar() {
         <li><Link href="/">Home</Link></li>
         <li><Link href="/products">Products</Link></li>
         <li><Link href="/cart">Cart</Link></li>
-        <li><Link href="/inventory">Inventory</Link></li>
+        {user?.role === 'admin' && (
+          <li><Link href="/inventory">Inventory</Link></li>
+        )} {/*we have a condition to only show this if an admin is loggedin*/}
         <li><Link href="/login">Login</Link></li>
       </ul>
     </nav>
